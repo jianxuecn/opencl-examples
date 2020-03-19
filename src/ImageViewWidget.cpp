6491ea1fa969267334d97138d300bdca9350deae
Jian Xue <xuejian@ucas.ac.cn>
2020-03-19 09:10:48 +0800

    fix bug for win

--- src/ImageViewWidget.cpp
+++ src/ImageViewWidget.cpp
@@ -3,6 +3,9 @@
 #include <QPixmap>
 #include <QImage>
 
+int const IMAGE_DISPLAY_WIDTH = 1920;
+int const IMAGE_DISPLAY_HEIGHT = 1080;
+
 ImageViewWidget::ImageViewWidget(QString const &title, QWidget *parent) :
     ToolWindow(parent),
     ui(new Ui::ImageViewWidget)
@@ -21,8 +24,8 @@ ImageViewWidget::~ImageViewWidget()
 void ImageViewWidget::showImage(QImage const &img)
 {
     this->setWindowTitle(QString("%1 (%2 x %3)").arg(mTitle).arg(img.width()).arg(img.height()));
-    if (img.width()>1024 || img.height()>1024) {
-        ui->labelImage->setPixmap(QPixmap::fromImage(img.scaled(1024, 1024, Qt::KeepAspectRatio)));
+    if (img.width()> IMAGE_DISPLAY_WIDTH || img.height()> IMAGE_DISPLAY_HEIGHT) {
+        ui->labelImage->setPixmap(QPixmap::fromImage(img.scaled(IMAGE_DISPLAY_WIDTH, IMAGE_DISPLAY_HEIGHT, Qt::KeepAspectRatio)));
     } else {
         ui->labelImage->setPixmap(QPixmap::fromImage(img));
     }
